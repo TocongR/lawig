d559867edede56fb9a5bb43cfbde99117140980b
Ralphjan Tocong <tocongralphjan@gmail.com>
2026-05-10 02:14:46 +0800

    pwa compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Lawig',
+        short_name: 'Lawig',
+        icons: [
+          { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+};
